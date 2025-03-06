5527cf0f523e64a367e5ccf5deeb461d7940ce61
afifmunir77 <afif.edu2018@gmail.com>
2025-03-06 17:00:53 +0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,23 @@
 // service-worker.js
-self.addEventListener('install', (event) => {
-  console.log('Service Worker installing');
-});
+const CACHE_NAME = 'photo-widget-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
 
-self.addEventListener('activate', (event) => {
-  console.log('Service Worker activating'); 
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
 self.addEventListener('fetch', (event) => {
-  // Basic fetch handler
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
